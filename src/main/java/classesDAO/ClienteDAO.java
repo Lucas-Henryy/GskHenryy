02ce131319b48311fcd79314f31ccee7a9708adc
Lucas Henrique Celestino <lucashenrique523146@gmail.com>
2025-10-29 15:24:17 -0300

    Refactor(DAO): aplicando o principio da responsabilidade única (SOLID)

--- src/main/java/classesDAO/ClienteDAO.java
+++ src/main/java/classesDAO/ClienteDAO.java
@@ -1,101 +1,63 @@
-
 package classesDAO;
 
-import classes.Cliente;
 import classes.JPAUtil;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
-import java.util.ArrayList;
-import java.util.List;
-import validacoes.Alerta;
 
 public class ClienteDAO {
-    public static void cadastrarCliente(Cliente cliente) {
-        EntityManager em = JPAUtil.getEntityManager();
-        try {
-            em.getTransaction().begin();
-            em.persist(cliente);
-            em.getTransaction().commit();
-            Alerta.Erro("Cadastro concluído!", "Cliente cadastrado com sucesso!");
 
-        } catch (Exception e) {
-            em.getTransaction().rollback();
-            Alerta.Erro("Erro no cadastro", "Erro ao inserir o cadastro no banco");
-        } finally {
-            JPAUtil.closeEntityManager();
-        }
-    }
-    
-   
-    public static List<Cliente> listarClientes(String cpf) {
-        EntityManager em = JPAUtil.getEntityManager();
-        List<Cliente> listaCli = new ArrayList<>();
-        Query consulta;
-
-        try {
-            if (cpf.equals("")) {
-                consulta = em.createQuery("SELECT cliente FROM Cliente cliente");
-                listaCli = consulta.getResultList();
-            } else {
-                consulta = em.createQuery("SELECT c from Cliente c WHERE c.cpf = :cpf");
-                consulta.setParameter("cpf", cpf);
-                listaCli = consulta.getResultList();
-            }
-
-            return listaCli;
-        } catch (Exception e) {
-            Alerta.Erro("Erro listagem", "Erro as buscar informação para lista");
-        }
-
-        return listaCli;
+    public void salvar(Cliente cliente) {
+        executarTransacao(em -> em.persist(cliente));
     }
 
-    public static Cliente listarCliente(String idCliente) {
-        EntityManager em = JPAUtil.getEntityManager();
-        Cliente cliente = null;
-
-        try {
-            cliente = em.find(Cliente.class, idCliente);
-            return cliente;
-        } catch (Exception e) {
-            Alerta.Erro("Erro", "Erro ao listar o cliente.");
-        } finally {
-            em.close();
-        }
-        return cliente;
+    public void atualizar(Cliente cliente) {
+        executarTransacao(em -> em.merge(cliente));
     }
-    
-    public static void editarCliente(Cliente cliente) {
-        EntityManager em = JPAUtil.getEntityManager();
+
+    public Cliente buscarPorId(String id) {
+        executarTransacao(em = JPAUtil.getEntityManager());
 
         try {
-            em.getTransaction().begin();
-            em.merge(cliente);
-            em.getTransaction().commit();
-            Alerta.Erro("Sucesso!", "Edição realizada com sucesso!");
+            return em.find(Cliente.class, id);
 
-        } catch (Exception e) {
-            em.getTransaction().rollback();
-            Alerta.Erro("Erro ao editar", "Ocorreu um erro ao editar as informações");
         } finally {
             em.close();
         }
     }
-    
-    public static void excluirClientes(String id) {
+
+    public list<cliente> listarTodos() {
         EntityManager em = JPAUtil.getEntityManager();
-
         try {
-            em.getTransaction().begin();
-            Cliente clientesRemover = em.find(Cliente.class, id);
-            em.remove(clientesRemover);
-            em.getTransaction().commit();
-
-        } catch (Exception e) {
-            Alerta.Erro("Erro excluir", "Erro ao excluir o cliente no banco");
+            TypedQuery<cliente> query = em.createQuery("SELECT c FROM Cliente c", cliente.class);
+            return query.getResultList();
         } finally {
-            JPAUtil.closeEntityManager();
+            em.close();
+        }
+
+    }
+
+    public list<cliente> buscarPorCPF() {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            TypedQuery<cliente> query = em.createQuery("SELECT c FROM Cliente c WHERE c.cpf :cpf", cliente.class);
+            return query.getResultList();
+        } finally {
+            em.close();
         }
     }
+
+    private void executarTransacao(java.util.function.Consumer<EntityManager> acao) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            acao.accept(em);
+            em.getTransaction().rollback();
+        } catch (RuntimeException e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+
+    }
+
 }
