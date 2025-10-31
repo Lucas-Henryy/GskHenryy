d444bbd271b68c3f1c0a5c46b2e9a116a4474bb5
Lucas Henrique Celestino <lucashenrique523146@gmail.com>
2025-10-31 16:54:37 -0300

    refactor: adicionar criação do método excluir o Funcionario

--- src/main/java/classesDAO/FuncionarioDAO.java
+++ src/main/java/classesDAO/FuncionarioDAO.java
@@ -1,117 +1,92 @@
 package classesDAO;
 
-import classes.Funcionario;
 import classes.Cargo;
+import classes.Funcionario;
 import classes.JPAUtil;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 import java.util.ArrayList;
 import java.util.List;
-import validacoes.Alerta;
 
 public class FuncionarioDAO {
-    
-    public static List<Cargo> pegarCargos(){
-        EntityManager em = JPAUtil.getEntityManager();
-        List<Cargo> cargos = new ArrayList<>();
-        try{
-            Query consulta = em.createQuery("SELECT c FROM Cargo c", Cargo.class);
-            cargos = consulta.getResultList();
-            return cargos;
-        } catch(Exception e){
-             Alerta.Erro("Erro ao consultar os cargos", "Erro de consulta");
-    }
-        return cargos;
-  }
-    public static void cadastrarFuncionario(Funcionario funcionario){
-        EntityManager em = JPAUtil.getEntityManager();
-        try {
-            em.getTransaction().begin();
-            em.persist(funcionario);
-            em.getTransaction().commit();
-            Alerta.Erro("Cadastro concluído!", "Funcionario cadastrado com sucesso!");            
-            
-        } catch (Exception e) {
-            em.getTransaction().rollback();
-            Alerta.Erro("Erro ao inserir o cadastro no banco", "Erro no cadastro");
-        } finally {
-            JPAUtil.closeEntityManager();
-        }
-    }
-    
-    public static List<Funcionario> listarFuncionarios(String cpf){
-        EntityManager em = JPAUtil.getEntityManager();
-        List<Funcionario> listaFunc = new ArrayList<>();
-        Query consulta;
 
-        try {
-            if (cpf.equals("")) {
-                consulta = em.createQuery("SELECT funcionario FROM Funcionario funcionario");
-                listaFunc = consulta.getResultList();
-            } else {
-                consulta = em.createQuery("SELECT f from Cliente f WHERE f.cpfC = :cpfF");
-                consulta.setParameter("cpfF", cpf);
-                listaFunc = consulta.getResultList();
-            }
-            return listaFunc;
-        } catch (Exception e) {
-            Alerta.Erro("Erro listagem", "Erro as buscar informação para lista");
-        }
-        return listaFunc;
+    public void salvar(Funcionario funcionario) {
+        executarTransacao(em -> em.persist(funcionario));
     }
-    
-    public static Funcionario listarFuncionario(String idFuncionario) {
-    EntityManager em = JPAUtil.getEntityManager();
-    Funcionario funcionario = null;
 
-    try {
-        Query query = em.createQuery(
-            "SELECT f FROM Funcionario f LEFT JOIN FETCH f.login WHERE f.id = :id",
-            Funcionario.class
-        );
-        query.setParameter("id", Long.parseLong(idFuncionario));
-        funcionario = (Funcionario) query.getSingleResult();
-        return funcionario;
-    } catch (Exception e) {
-        Alerta.Erro("Erro ao listar", "Erro ao listar o funcionario");
-    } finally {
-        em.close();
+    public void atualizar(Funcionario cliente) {
+        executarTransacao(em -> em.merge(cliente));
     }
-    return funcionario;
-}
-    
-    public static void editarFuncionario(Funcionario funcionario) {
+
+    public Funcionario buscarPorId(String id) {
         EntityManager em = JPAUtil.getEntityManager();
 
         try {
-            em.getTransaction().begin();
-            em.merge(funcionario);
-            em.getTransaction().commit();
-            Alerta.Erro("Sucesso!", "Edição realizada com sucesso!");
+            return em.find(Funcionario.class, id);
 
-        } catch (Exception e) {
-            em.getTransaction().rollback();
-            Alerta.Erro("Erro ao editar", "Ocorreu um erro ao editar as informações do cliente.");
         } finally {
             em.close();
         }
     }
-    
-    public static void excluirFuncionario(String id) {
+
+    public List<Funcionario> listarTodos() {
         EntityManager em = JPAUtil.getEntityManager();
-
         try {
-            em.getTransaction().begin();
-            Funcionario funcionariosRemover = em.find(Funcionario.class, id);
-            em.remove(funcionariosRemover);
-            em.getTransaction().commit();
-
-        } catch (Exception e) {
-            Alerta.Erro("Erro excluir", "Erro ao excluir o cliente no banco");
+            TypedQuery<Funcionario> query = em.createQuery("SELECT c FROM Cliente c", Funcionario.class);
+            return query.getResultList();
         } finally {
-            JPAUtil.closeEntityManager();
+            em.close();
+        }
+
+    }
+
+    public List<Funcionario> buscarPorCPF(String cpf) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            TypedQuery<Funcionario> query = em.createQuery("SELECT c FROM Cliente c WHERE c.cpfF :cpfF", Funcionario.class);
+            return query.getResultList();
+        } finally {
+            em.close();
         }
     }
-    
-    
+
+    public List<Cargo> ListarCargos(String cargo) {
+        EntityManager em = JPAUtil.getEntityManager();
+        List<Cargo> cargos = new ArrayList<>();
+        try {
+            TypedQuery<Cargo> query = em.createQuery("SELECT c FROM Cargo c", Cargo.class);
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void excluirFuncionario(String id) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            Funcionario cliente = em.find(Funcionario.class, id);
+            if (cliente != null) {
+                em.remove(cliente);
+            }
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    private void executarTransacao(java.util.function.Consumer<EntityManager> acao) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            acao.accept(em);
+            em.getTransaction().rollback();
+        } catch (RuntimeException e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+
+    }
+
 }
