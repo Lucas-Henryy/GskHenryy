c245f796a44d4abe630e6d47df8a71bcf02ff145
Lucas Henrique Celestino <lucashenrique523146@gmail.com>
2025-10-31 16:48:45 -0300

    refactor: adicionar metodo de excluir os clientes

--- src/main/java/services/ClienteService.java
+++ src/main/java/services/ClienteService.java
@@ -12,42 +12,52 @@ public class ClienteService {
     public void cadastrarCliente(Cliente cliente) {
 
         try {
-            clienteDAO.salvar(cliente)
+            clienteDAO.salvar(cliente);
             Alerta.Sucesso("Cadastro concluído!!", "Cliente cadastrado com sucesso");
         } catch (Exception e) {
-            Alerta.Erro("Erro ao editar", "Ocorreu um erro ao editar as informações");
+            Alerta.Erro("Erro ao Cadastrar", "Ocorreu um erro ao cadastrar o cliente");
         }
     }
 
     public void editarCliente(Cliente cliente) {
 
         try {
-            clienteDAO.atualizar(cliente)
+            clienteDAO.atualizar(cliente);
             Alerta.Sucesso("Sucesso!", "Edição realizada com sucesso");
 
         } catch (Exception e) {
-            Alerta.Erro("Erro ao editar", "Ocorreu um erro ao editar as infromacoes");
+            Alerta.Erro("Erro ao editar", "Ocorreu um erro ao editar as infromações");
         }
     }
 
     public List<Cliente> listarClientes(String cpf) {
         try {
-            if (cpf == null || cpf.isEmpty()) {
+            if(cpf == null || cpf.isEmpty()) {
                 return clienteDAO.listarTodos();
             }
-            return clienteDAO.buscarPorCpf(cpf)
+            return clienteDAO.buscarPorCPF(cpf);
         } catch (Exception e) {
             Alerta.Erro("Erro listagem", "Erro ao buscar informacoes para lista");
             return List.of();
         }
     }
     
+        public void excluirCliente(String idCliente) {
+
+        try {
+            clienteDAO.excluir(idCliente);
+            Alerta.Sucesso("Exclusão realizada!", "Exclusão realizada com sucesso");
+        } catch (Exception e) {
+            Alerta.Erro("Erro ao excluir", "Ocorreu um erro ao excluir as informações");
+        }
+    }
+    
     public Cliente buscarCliente (String idCliente){
         
         try {
-            return clienteDAO.buscarPorId(idCliente)
+            return clienteDAO.buscarPorId(idCliente);
         } catch (Exception e) {
-            Alerta.Erro("Erro", "Erro ao listar o cliente");
+            Alerta.Erro("Erro", "Erro ao buscar o cliente");
             return null;
         }
     } 
