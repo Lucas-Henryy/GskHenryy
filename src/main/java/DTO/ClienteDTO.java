c5ba51ee0a52a95d9859fee17f29e6a10c8e2ed4
Lucas Henrique Celestino <lucashenrique523146@gmail.com>
2025-12-02 16:13:04 -0300

    feat: criação dos DTOs

---
+++ src/main/java/DTO/ClienteDTO.java
@@ -0,0 +1,62 @@
+package DTO;
+
+public class ClienteDTO {
+
+    private String nome;
+    private String cpf;
+    private String sexo;
+    private String telefone;
+    private String email;
+
+    public ClienteDTO(String nome, String cpf, String sexo, String telefone, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.sexo = sexo;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+   
+    
+    
+}
